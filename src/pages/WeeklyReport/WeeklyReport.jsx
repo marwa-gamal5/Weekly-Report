1550cb38917f750998252eefdf22cc3e9318231c
Marwa Gamal <marwagamal443@gmail.com>
2023-09-14 16:07:56 +0300

    v4

--- src/pages/WeeklyReport/WeeklyReport.jsx
+++ src/pages/WeeklyReport/WeeklyReport.jsx
@@ -4,7 +4,7 @@ import logoImg3 from '../../assets/logoImg3.png';
 import Table from 'react-bootstrap/Table';
 import './WeeklyReport.css';
 import React, { useRef } from 'react';
-import JsPDF from 'jspdf';
+import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
@@ -149,41 +149,234 @@ function WeeklyReport() {
 
 
 
-    const options = {
-        // default is `save`
-        method: 'open',
+    // const options = {
+    //     // default is `save`
+    //     method: 'open',
        
-        resolution: Resolution.HIGH,
-        page: {
-           // margin is in MM, default is Margin.NONE = 0
-           margin: Margin.SMALL,
-           // default is 'A4'
-           format: 'B4',
-           // default is 'portrait'
-           orientation: 'landscape',
-        },
+    //     resolution: Resolution.HIGH,
+    //     page: {
+    //        // margin is in MM, default is Margin.NONE = 0
+    //        margin: Margin.SMALL,
+    //        // default is 'A4'
+    //        format: 'B4',
+    //        // default is 'portrait'
+    //        orientation: 'landscape',
+    //     },
        
        
        
-     };
+    //  };
      
-     // you can use a function to return the target element besides using React refs
-     const getTargetElement = () => document.getElementById('content-id');
+    //  // you can use a function to return the target element besides using React refs
+    //  const getTargetElement = () => document.getElementById('content-id');
+   // Function to generate the PDF
+   const generatePDF = () => {
+    // Create a new jsPDF instance with landscape orientation
+    const doc = new jsPDF();
 
+    // Add the content to the PDF
+    // You can use doc.text, doc.image, etc. to add content
+    //row one
+    doc.addImage(logoImg3, 'PNG', 4, 10,); // Add Logo3 image
+    doc.setFont('Poppins', '600');
+    doc.setFontSize(23.054);
+    doc.text(' Activity Report', 73, 18);
+    doc.setFontSize(17.89);
+    doc.setTextColor('#089BD9'); // Set text color to blue
+    doc.text(`From ${startDate} To ${endDate}`, 55, 30);
+    // doc.text('From 31 / 8/ 2023 To 6/9/2023', 110, 40);
+    doc.setTextColor(0); // Reset text color to black
+    doc.addImage(logoImg2, 'PNG', 185, 10, ); // Add Logo1 image
+    doc.addImage(logoImg1, 'PNG', 160, 10,); // Add Logo2 image
+
+     //row two 
+// Adding "Total Visitor" section
+doc.setFillColor(214, 235, 255); // Set background color
+doc.rect(10,50, 60, 30, 'F'); // Draw a filled rectangle
+
+doc.setFont('Poppins', '600');
+doc.setFontSize(20);
+doc.setTextColor(9, 98, 136); // Set text color to a shade of blue
+doc.text('Total Visitor', 14, 62); // Add the "Total Visitor" text
+
+doc.setFont('Inter', '1000');
+doc.setFontSize(20);
+doc.setTextColor(0); // Reset text color to black
+doc.text(`${sumOfTitleNumbers}`, 14, 77); // Add the visitor count
+
+// Adding "Total Registrations" section
+doc.setFillColor(214, 235, 255); // Set background color
+doc.rect(75,50, 60, 30, 'F'); // Draw a filled rectangle
+
+doc.setFont('Poppins', '600');
+doc.setFontSize(20);
+doc.setTextColor(9, 98, 136); // Set text color to a shade of blue
+doc.text('Total Registrations', 78, 62); // Add the "Total Registrations" text
+
+doc.setFont('Inter', '1000');
+doc.setFontSize(20);
+doc.setTextColor(0); // Reset text color to black
+doc.text(`${sumOfCourseRegistrations}`, 78, 77); // Add the visitor count
+
+// // Adding "Total Payment" section
+doc.setFillColor(214, 235, 255); // Set background color
+doc.rect(140,50, 60, 30, 'F'); // Draw a filled rectangle
+
+doc.setFont('Poppins', '600');
+doc.setFontSize(20);
+doc.setTextColor(9, 98, 136); // Set text color to a shade of blue
+doc.text('Total Payment', 143, 62); // Add the "Total Payment" text
+
+doc.setFont('Inter', '1000');
+doc.setFontSize(20);
+doc.setTextColor(0); // Reset text color to black
+doc.text(`${sumOfCourseRegistrations1}`, 143, 77); // Add the visitor count
+
+
+//row three
+
+const sectionThreeYStart = 100; // Adjust this value as needed
+
+// // Visitor By Continent
+// doc.setFont('Poppins', '600');
+// doc.setFontSize(18);
+// doc.setTextColor(9, 98, 136);
+// doc.text('Visitor By Continent', 115, sectionThreeYStart - 1);
+
+// Visitor By Continent Table
+const tableData1 = [
+    ['Continent', 'Total Users'],
+    ...titles.map((title) => [title[0], title[1]]),
+];
+
+const table1Options = {
+    startY: sectionThreeYStart + 4,
+   
+    headStyles: {
+        fontSize: 10,
+        textColor: [0, 0, 0],
+        cellPadding: 6,
+        halign: 'center',
+        fillColor: [244, 244, 244], // Background color for header cells
+    },
+    bodyStyles: {
+        fontSize: 10,
+        textColor: [0, 0, 0],
+        cellPadding: 6,
+        halign: 'center',
+        fillColor: [255, 255, 255], // Background color for body cells (white)
+        lineWidth: 0,
+    },
+    theme: 'grid',
+    tableWidth: 'auto', // Adjust the width as needed
+    tableHeight: 'auto', // Adjust the height as needed
+};
+
+// doc.autoTable(tableData1[0], tableData1.slice(1), table1Options);
+doc.autoTable({
+    head: tableData1.slice(0, 1),
+    body: tableData1.slice(1),
+    startY: 100,
+  });
+// // Course Registrations
+// doc.setFont('Poppins', '600');
+// doc.setFontSize(18);
+// doc.setTextColor(9, 98, 136);
+// doc.text('Course Registrations',115, sectionThreeYStart - 10); // Adjust vertical position
+
+// Course Registrations Table
+const tableData2 = [
+    ['Course Registrations', 'NUM'],
+    ...CourseRegistrations.map((CourseRegistration) => [CourseRegistration[0], CourseRegistration[2]]),
+];
+doc.autoTable({
+    head: tableData2.slice(0, 1),
+    body: tableData2.slice(1),
+    startY: doc.previousAutoTable.finalY + 10,
+  });
+
+// const table2Options = {
+//     startY: sectionThreeYStart + 10, // Adjust vertical position
+//     // startX: 250,
+//     headStyles: {
+//         fontSize: 10,
+//         textColor: [0, 0, 0],
+//         cellPadding: 6,
+//         halign: 'center',
+//         fillColor: [244, 244, 244], // Background color for header cells
+//     },
+//     bodyStyles: {
+//         fontSize: 10,
+//         textColor: [0, 0, 0],
+//         cellPadding: 6,
+//         halign: 'center',
+//         fillColor: [255, 255, 255], // Background color for body cells (white)
+//         lineWidth: 0,
+//     },
+//     theme: 'grid',
+//     tableWidth: 'auto', // Adjust the width as needed
+//     tableHeight: 'auto', // Adjust the height as needed
+// };
+
+// doc.autoTable(tableData2[0], tableData2.slice(1), table2Options);
+
+// // Course Payment
+// doc.setFont('Poppins', '600');
+// doc.setFontSize(18);
+// doc.setTextColor(9, 98, 136);
+// doc.text('Course Payment', 115, sectionThreeYStart +85); // Adjust vertical position
+
+// Course Payment Table
+const tableData3 = [
+    ['Course Payments', 'NUM'],
+    ...CourseRegistrations.map((CourseRegistration) => [CourseRegistration[0], CourseRegistration[1]]),
+];
+
+doc.autoTable({
+    head: tableData3.slice(0, 1),
+    body: tableData3.slice(1),
+    startY: doc.previousAutoTable.finalY + 10,
+  });
+// const table3Options = {
+//     startY: sectionThreeYStart + 80, // Adjust vertical position
+//     // startX: 450,
+//     headStyles: {
+//         fontSize: 10,
+//         textColor: [0, 0, 0],
+//         cellPadding: 6,
+//         halign: 'center',
+//         fillColor: [244, 244, 244], // Background color for header cells
+//     },
+//     bodyStyles: {
+//         fontSize: 10,
+//         textColor: [0, 0, 0],
+//         cellPadding: 6,
+//         halign: 'center',
+//         fillColor: [255, 255, 255], // Background color for body cells (white)
+//         lineWidth: 0,
+//     },
+//     theme: 'grid',
+//     tableWidth: 'auto', // Adjust the width as needed
+//     tableHeight: 'auto', // Adjust the height as needed
+// };
+
+// doc.autoTable(tableData3[0], tableData3.slice(1), table3Options);
+    // Save or download the PDF
+    doc.save('weekly_report.pdf');
+  };
     return (<>
         <div>
         <button onClick={handleSubmit}>Submit</button>
           
-            {/* <button onClick={generatePDF} type="button">
-                Export PDF
-            </button> */}
-              <button onClick={() => generatePDF(getTargetElement, options)}>Generate PDF</button>
+           
+            <button onClick={generatePDF}>Generate PDF</button>
+             
               <div id="content-id">
              <div id="report"  >
                 <div id="sectionone">
-                    <div className='row  m-5 d-flex  align-items-center '>
-                        <div className='col-2'>
-                            <img src={logoImg3} alt="Logo3" />
+                    <div className='row  m-5 d-flex   align-items-center justify-content-center '>
+                        <div className='col-2  '>
+                            <img  className='logoImg3'src={logoImg3} alt="Logo3" />
                         </div>
 
                         <div className="p col-8 ">
@@ -200,7 +393,7 @@ function WeeklyReport() {
                         </div>
 
 
-                        <div className='col-2  d-flex align-items-center justify-content-end '>
+                        <div className='col-2  d-flex align-items-center justify-content-end  '>
                             <div><img className='logoImg1 p-3 ' src={logoImg1} alt="Logo1" /></div>
                             
                           <div> <img className='logoImg2 p-3' src={logoImg2} alt="Logo2" /></div>
@@ -212,6 +405,10 @@ function WeeklyReport() {
 
                     </div>
                 </div>
+
+
+
+
                 <div id="sectiontwo">
                     <div className='row m-4'>
                         <div className=' col-4 d-flex align-items-center justify-content-center  '>
@@ -363,6 +560,7 @@ function WeeklyReport() {
                         </div>
                     </div>
                 </div>
+
 
                 {/* <div id="sectionfour">
                      <div className='row  fixed-bottom '>
